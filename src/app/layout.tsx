0ec6be0d741c134ded55d66ea0c0ab432cef2561
hatimhbch <158487800+hatimhbch@users.noreply.github.com>
2024-08-10 18:19:01 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,8 @@ export default function RootLayout({
       <Head>
         <link rel="icon" href="/favicon.png" />
         <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="../../../favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="../../../favicon-16x16.png" />
         <script type="application/ld+json">
           {JSON.stringify(jsonLdLayout)}
         </script>
