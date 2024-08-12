799685893ce43af49f03d7bc6baa449549142ae6
hatimhbch <158487800+hatimhbch@users.noreply.github.com>
2024-08-12 14:56:02 +0100

    jj

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://elevenai.co',
-    generateRobotsTxt: true,
-    // optional
-    robotsTxtOptions: {
-      policies: [{ userAgent: '*', allow: '/' }],
-    },
-  };
+  siteUrl: process.env.SITE_URL || 'https://elevenai.co',
+  generateRobotsTxt: true,
+  // optional
+  robotsTxtOptions: {
+    policies: [{ userAgent: '*', allow: '/' }],
+  },
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**',
-        },
-      ],
-    },
-    distDir: 'build',
-  };
-  
-  export default nextConfig;  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  distDir: 'build',
+};
+
+export default nextConfig;  
