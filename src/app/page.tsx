e62d62544b9f3011874c3987f4bf42562167d2f4
hatimhbch <158487800+hatimhbch@users.noreply.github.com>
2024-08-17 00:55:31 +0100

    update home page metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import { Metadata } from "next";
 import Aboutme from "./components/aboutme/page";
 import Collection from "./components/collection/collection";
 import '@/app/page.css'
+
+export const metadata: Metadata = {
+  title: "elevenai is Blog for Articles about anything you think about. ai and programming languages",
+}
 
 export default function Home() {
   return (
